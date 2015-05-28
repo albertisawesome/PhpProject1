bea9efab296900319247b341d63a27b71948bb20
Albert Chen <chenalbert60@gmail.com>
2015-05-28 14:45:27 -0700

    qwerqwerqwerqwe

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>coolabah</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+      <style>body{background-image: url('');}</style>
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+  <link type="text/css" rel="stylesheet" href="newCascadeStyleSheet.css">
+  
+ 
+</head>
+<body>
+
+<div class="container">
+  <div class="jumbotron">
+ <html>
+<head>
+<style>
+h1 {text-align:center;}
+h2 {text-align:center;}
+p {text-align:center;}
+</style>
+</head>
+<body>
+
+<h1><font color="grey">CNN</font></h1>
+<h2><font color="grey">Newsweekly</font></h2>
+
+</body>
+</html>
+
+  </div>
+   
+  <div class="row">
+    <div class="col-sm-4">
+       <a href='http://www.cnn.com/' class='button'>Home</a>
+        <h1><font color="white">Your personal CNN.</font></h1>
+      <p>hello...</p>
+      <p>bye...</p>
+    </div>
+    
+    <div class="col-sm-4">
+          <a href='haha.php' class='button'>Hi</a>
+     <h1><font color="white">Your personal CNN.</font></h1>
+      <p>weeeee...</p>
+      <p>beeee...</p>
+    </div>
+      <a href='haha.php' class='button'>Hi</a>
+    <div class="col-sm-4">
+    <h1><font color="white">Your personal CNN.</font></h1>       
+      <p>bola...</p>
+      <p>bruh...</p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
+
+
+
+
+<html>
+  <body>
+  <div class="nav">
+    <div class="container">
+      
+	<u
+</html>l>
+	  <li>Albert</li>
+	  <li>Browse</li>
+	</ul>
+	<ul><li>
+	Sign Up
+	<li>
+	Log In
+	</li>
+	<li>
+	Help
+	</li>
+	</li></ul>
+	
+	</div>
+    <div class="jumbotron">
+    <div>
+    <h3></h3>
+    
+     <p></p>
+     <p> <a href="#"></a></p>
+   </div>
+    
+        <div class="container">
+	
+        <h1>Breaking News.</h1>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/MnR9AVs6Q_c" frameborder="0" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/SxKzGu8fQEA" frameborder="0" allowfullscreen></iframe>
+	<p> <a href="#">See what is going on now.</a></p>
+	</div>
+	
+  </body> random music ------------------------->
+    
+    
+    <audio controls>
+        <source src="audio/AdventureQuestWorldGileadTheme.mp3" type="audio/mpeg">
+    </audio>
+    
+<!-- start feedwind code --><script type="text/javascript">document.write('\x3Cscript type="text/javascript" src="' + ('https:' == document.location.protocol ? 'https://' : 'http://') + 'feed.mikle.com/js/rssmikle.js">\x3C/script>');</script><script type="text/javascript">(function() {var params = {rssmikle_url: "http://rss.cnn.com/rss/edition.rss",rssmikle_frame_width: "300",rssmikle_frame_height: "400",frame_height_by_article: "0",rssmikle_target: "_blank",rssmikle_font: "Arial, Helvetica, sans-serif",rssmikle_font_size: "12",rssmikle_border: "off",responsive: "off",rssmikle_css_url: "",text_align: "left",text_align2: "left",corner: "off",scrollbar: "on",autoscroll: "on",scrolldirection: "up",scrollstep: "3",mcspeed: "20",sort: "Off",rssmikle_title: "on",rssmikle_title_sentence: "",rssmikle_title_link: "",rssmikle_title_bgcolor: "#0066FF",rssmikle_title_color: "#FFFFFF",rssmikle_title_bgimage: "",rssmikle_item_bgcolor: "#FFFFFF",rssmikle_item_bgimage: "",rssmikle_item_title_length: "55",rssmikle_item_title_color: "#0066FF",rssmikle_item_border_bottom: "on",rssmikle_item_description: "on",item_link: "off",rssmikle_item_description_length: "150",rssmikle_item_description_color: "#666666",rssmikle_item_date: "gl1",rssmikle_timezone: "Etc/GMT",datetime_format: "%b %e, %Y %l:%M:%S %p",item_description_style: "text+tn",item_thumbnail: "full",item_thumbnail_selection: "auto",article_num: "15",rssmikle_item_podcast: "off",keyword_inc: "",keyword_exc: ""};feedwind_show_widget_iframe(params);})();</script><div style="font-size:10px; text-align:center; width:300px;"><a href="http://feed.mikle.com/" target="_blank" style="color:#CCCCCC;">RSS Feed Widget</a><!--Please display the above link in your web page according to Terms of Service.--></div><!-- end feedwind code -->
